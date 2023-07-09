c5110cf0cf2f17c2a21b1aad65b631459f7b650a
PCuser <2310216798@qq.com>
2023-07-09 11:33:11 +0800

    2

--- src/main/java/com/example/shop/ShopApplication.java
+++ src/main/java/com/example/shop/ShopApplication.java
@@ -1,13 +1,39 @@
 package com.example.shop;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+
+@Configuration
 @SpringBootApplication
-public class ShopApplication {
+@MapperScan("com.java.bfmk.mapper")
+public class ShopApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  后台启动成功   ლ(´ڡ`ლ)ﾞ  \n" );
+    }
+
+
+
+//    @Bean
+//    public MybatisPlusInterceptor page() {
+//        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+//        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+//        return interceptor;
+//    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/shop/**").addResourceLocations("file:F:/create/shop/");
     }
 
 }
